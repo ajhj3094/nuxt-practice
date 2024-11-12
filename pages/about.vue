076308f1789d6940693c10deea7c422519c16545
Justin Yu <ajhj3094@eip.chuanhwa.com.tw>
2024-11-12 16:52:11 +0800

    feat: reusable components

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      repellendus ullam pariatur voluptatibus voluptates atque rerum accusantium
+      eum. Assumenda quisquam aliquid modi hic sequi quos neque, similique
+      beatae quidem laborum!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      repellendus ullam pariatur voluptatibus voluptates atque rerum accusantium
+      eum. Assumenda quisquam aliquid modi hic sequi quos neque, similique
+      beatae quidem laborum!
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+h2 {
+  margin-top: 2rem;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ProductDetails :product="product"></ProductDetails>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+
+const { data: product } = await useFetch(uri);
+
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style lang="scss" scoped></style>
